b4da0c6f23b9a1d9a92a893dc9c74a992691da71
Salah Eddine Boukadi <bkdsalah@gmail.com>
2021-04-19 13:08:35 +0000

    add pagination & search features

--- src/main/java/me/boukadi/springdatatp/demo/dao/PatientRepository.java
+++ src/main/java/me/boukadi/springdatatp/demo/dao/PatientRepository.java
@@ -1,10 +1,13 @@
 package me.boukadi.springdatatp.demo.dao;
 
 import me.boukadi.springdatatp.demo.entity.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-    Patient findByNom(String nom);
+    Page<Patient> findByNomContains(String patientName, Pageable pageable);
 }
--- src/main/java/me/boukadi/springdatatp/demo/web/PatientController.java
+++ src/main/java/me/boukadi/springdatatp/demo/web/PatientController.java
@@ -3,9 +3,12 @@ package me.boukadi.springdatatp.demo.web;
 import me.boukadi.springdatatp.demo.dao.PatientRepository;
 import me.boukadi.springdatatp.demo.entity.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,9 +24,15 @@ public class PatientController {
     }
 
     @GetMapping("/patients")
-    public String GetPatients(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients", patients);
+    public String GetPatients(Model model,
+                              @RequestParam(name = "page", defaultValue = "0") int page,
+                              @RequestParam(name = "size", defaultValue = "5") int size,
+                              @RequestParam(name = "keyword", defaultValue = "") String patientName) {
+        Page<Patient> pagePatients = patientRepository.findByNomContains(patientName, PageRequest.of(page, size));
+        model.addAttribute("patients", pagePatients);
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("keyword", patientName);
         return "patients";
     }
 }
