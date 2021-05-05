fa5395a9d2750244c971c82c295e953b67e1f31c
Salah Eddine Boukadi <bkdsalah@gmail.com>
2021-05-05 12:22:07 +0000

    add update & delete features

--- src/main/java/me/boukadi/springdatatp/demo/entity/Patient.java
+++ src/main/java/me/boukadi/springdatatp/demo/entity/Patient.java
@@ -4,8 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -16,12 +20,21 @@ public class Patient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
     @Column(name = "nom")
+    @NotNull
+    @Size(min = 5, max = 15/*, message="my message"*/)
     private String nom;
+
     @Column(name = "date_naissance")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaisssance;
+
     @Column(name = "score")
+    @DecimalMin(value = "5")
     private int score;
+
     @Column(name = "malade")
     private boolean malade;
 }
--- src/main/java/me/boukadi/springdatatp/demo/web/PatientController.java
+++ src/main/java/me/boukadi/springdatatp/demo/web/PatientController.java
@@ -7,9 +7,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -32,7 +35,35 @@ public class PatientController {
         model.addAttribute("patients", pagePatients);
         model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
         model.addAttribute("currentPage", page);
+        model.addAttribute("size", size);
         model.addAttribute("keyword", patientName);
         return "patients";
     }
+
+    @GetMapping(path = "/formPatient")
+    public String formPatient(Model model) {
+        model.addAttribute("patient", new Patient());
+        return "formPatient";
+    }
+
+    @PostMapping(path = "/savePatient")
+    public String savePatient(@Valid Patient patient, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) return "formPatient";
+        patientRepository.save(patient);
+        return "patients";
+    }
+
+    @GetMapping(path = "/updatePatient")
+    public String savePatient(Model model, Long id) {
+        Patient patient = patientRepository.findById(id).get();
+        model.addAttribute("patient", patient);
+        return "patients";
+    }
+
+    @GetMapping(path = "/deletePatient")
+    public String deletePatient(Long id, String keyword, int page, int size) {
+        patientRepository.deleteById(id);
+        return "redirect:/patients?page=" + page + "&size=" + size + "&keyword=" + keyword;
+    }
+
 }
